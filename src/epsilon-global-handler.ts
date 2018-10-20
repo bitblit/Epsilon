6ac66a2c5a44c8f0d60e10a99983574c480e4e3d
Chris Weiss <bitblit@gmail.com>
2018-10-19 23:37:28 -0700

    Better logging

--- src/epsilon-global-handler.ts
+++ src/epsilon-global-handler.ts
@@ -55,6 +55,7 @@ export class EpsilonGlobalHandler {
             }
 
             if (LambdaEventDetector.isValidApiGatewayEvent(event)) {
+                Logger.debug('Epsilon: APIG: %j', event);
                 const wh: WebHandler = this.fetchWebHandler();
                 if (wh) {
                     rval = await wh.lambdaHandler(event as APIGatewayEvent);
@@ -62,6 +63,7 @@ export class EpsilonGlobalHandler {
                     Logger.warn('API Gateway event, but no handler or disabled');
                 }
             } else if (LambdaEventDetector.isValidSnsEvent(event)) {
+                Logger.debug('Epsilon: SNS: %j', event);
                 // If salt mine is here, it takes precedence
                 const sm: SaltMineHandler = this.fetchSaltMineHandler();
                 if (sm && sm.isSaltMineStartSnsEvent(event)) {
@@ -70,10 +72,16 @@ export class EpsilonGlobalHandler {
                     rval = await this.processSnsEvent(event as SNSEvent);
                 }
             } else if (LambdaEventDetector.isValidS3Event(event)) {
+                Logger.debug('Epsilon: S3: %j', event);
+
                 rval = await this.processS3Event(event as S3CreateEvent);
             } else if (LambdaEventDetector.isValidCronEvent(event)) {
+                Logger.debug('Epsilon: CRON: %j', event);
+
                 rval = await this.processCronEvent(event as ScheduledEvent);
             } else if (LambdaEventDetector.isValidDynamoDBEvent(event)) {
+                Logger.debug('Epsilon: DDB: %j', event);
+
                 rval = await this.processDynamoDbEvent(event as DynamoDBStreamEvent);
             } else {
                 Logger.warn('Unrecognized event, returning false : %j', event);
@@ -89,9 +97,12 @@ export class EpsilonGlobalHandler {
     private async processSnsEvent(evt: SNSEvent): Promise<any> {
         let rval: any = null;
         if (this.config && this.config.sns && !this.config.disabled.sns && evt && evt.Records.length>0) {
-            const handler: SnsHandlerFunction = this.config.sns.handlers.get(evt.Records[0].Sns.TopicArn);
+            const finder: string = evt.Records[0].Sns.TopicArn;
+            const handler: SnsHandlerFunction = this.findInMap<SnsHandlerFunction>(finder, this.config.sns.handlers);
             if (handler) {
                 rval = await handler(evt);
+            } else {
+                Logger.info('Found no SNS handler for : %s', finder);
             }
         }
         return rval;
@@ -100,9 +111,12 @@ export class EpsilonGlobalHandler {
     private async processS3Event(evt: S3CreateEvent): Promise<any> {
         let rval: any = null;
         if (this.config && this.config.s3 && !this.config.disabled.s3 && evt && evt.Records.length>0) {
-            const handler: S3HandlerFunction = this.config.s3.handlers.get(evt.Records[0].s3.bucket.name);
+            const finder: string = evt.Records[0].s3.bucket.name;
+            const handler: S3HandlerFunction = this.findInMap<S3HandlerFunction>(finder, this.config.s3.handlers);
             if (handler) {
                 rval = await handler(evt);
+            } else {
+                Logger.info('Found no s3 handler for : %s', finder);
             }
         }
         return rval;
@@ -111,9 +125,12 @@ export class EpsilonGlobalHandler {
     private async processCronEvent(evt: ScheduledEvent): Promise<any> {
         let rval: any = null;
         if (this.config && this.config.cron && !this.config.disabled.cron && evt && evt.resources[0]) {
-            const handler: CronHandlerFunction = this.config.cron.handlers.get(evt.resources[0]);
+            const finder: string = evt.resources[0];
+            const handler: CronHandlerFunction = this.findInMap<CronHandlerFunction>(finder, this.config.cron.handlers);
             if (handler) {
                 rval = await handler(evt);
+            } else {
+                Logger.info('Found no Cron handler for : %s', finder);
             }
         }
         return rval;
@@ -122,11 +139,23 @@ export class EpsilonGlobalHandler {
     private async processDynamoDbEvent(evt: DynamoDBStreamEvent): Promise<any> {
         let rval: any = null;
         if (this.config && this.config.dynamoDb && !this.config.disabled.dynamoDb && evt && evt.Records && evt.Records.length > 0) {
-            const handler: DynamoDbHandlerFunction = this.config.dynamoDb.handlers.get(evt.Records[0].eventSourceARN);
+            const finder: string = evt.Records[0].eventSourceARN;
+            const handler: DynamoDbHandlerFunction = this.findInMap<DynamoDbHandlerFunction>(finder, this.config.dynamoDb.handlers);
             if (handler) {
                 rval = await handler(evt);
+            } else {
+                Logger.info('Found no Dynamo handler for : %s', finder);
             }
         }
+        return rval;
+    }
+
+    private findInMap<T>(toFind: string, map: Map<string, T>): T {
+        let rval: T = null;
+        map.forEach((val, key) => {
+            if (toFind.match(key)) {}
+            rval = val;
+        })
         return rval;
     }
 
