ddd51841a177442bfe1a5b41a025eb579a64fd4d
Chris Weiss <bitblit@gmail.com>
2020-01-22 16:50:46 -0500

    Try again with the files this time - Adding yaml combiner

---
+++ src/open-api-util/yaml-combiner.ts
@@ -0,0 +1,36 @@
+/*
+    Functions to combine a set of yaml files
+*/
+
+import * as fs from 'fs';
+import {RequireRatchet} from '@bitblit/ratchet/dist/common/require-ratchet';
+import * as yaml from 'node-yaml';
+import {Logger} from '@bitblit/ratchet/dist/common/logger';
+
+export class YamlCombiner {
+
+    public static combine(files: string[], inRootPath: string[] = []): string {
+        RequireRatchet.notNullOrUndefined(files, 'Files argument');
+        RequireRatchet.true(files.length > 0, 'Files argument larger than 0');
+        RequireRatchet.notNullOrUndefined(inRootPath, 'Root path argument');
+        Logger.info('Processing %d files into output', files.length);
+
+        let allElements: {} = {};
+        for (let i=0;i < files.length; i++) {
+            const fileContents: string = fs.readFileSync(files[i]).toString();
+            const openApi:any = yaml.parse(fileContents);
+            allElements = Object.assign(allElements, openApi);
+        }
+        let rootPath: string[] = Object.assign([], inRootPath);
+        while (rootPath.length > 0) {
+            const next: any = {};
+            next[rootPath[rootPath.length-1]]=allElements;
+            rootPath.splice(rootPath.length - 1,1);
+            allElements = next;
+        }
+
+        const rval: string = yaml.dump(allElements);
+        return rval;
+    }
+
+}
