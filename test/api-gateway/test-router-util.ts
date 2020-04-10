48d27e76f2dfc69cc8f7b6ce6a1efcacf69301fc
Chris Weiss <bitblit@gmail.com>
2020-04-09 19:14:24 -0700

    Fix test

--- test/api-gateway/test-router-util.ts
+++ test/api-gateway/test-router-util.ts
@@ -10,7 +10,7 @@ describe('#routerUtil', function() {
 
     it('should build default reflective cors handler', async () => {
         const evt: APIGatewayEvent = JSON.parse(fs.readFileSync('test/sample-json/sample-request-1.json').toString());
-        const proxy: ProxyResult = RouterUtil.defaultReflectiveCorsHandler(evt);
+        const proxy: ProxyResult = RouterUtil.defaultReflectiveCorsOptionsFunction(evt);
 
         expect(proxy.headers).to.not.be.null;
     });
