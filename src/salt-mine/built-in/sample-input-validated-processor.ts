2b39edc84840c9b29bf5e950099a3db5cd6bd33d
Chris Weiss <bitblit@gmail.com>
2021-07-16 12:12:22 -0700

    Adding new salt mine default handler for validation testing

---
+++ src/salt-mine/built-in/sample-input-validated-processor.ts
@@ -0,0 +1,44 @@
+import { Logger, PromiseRatchet } from '@bitblit/ratchet/dist/common';
+import { SaltMineConfig } from '../salt-mine-config';
+import { SaltMineNamedProcessor } from '../salt-mine-named-processor';
+
+export class SampleInputValidatedProcessor
+  implements SaltMineNamedProcessor<SampleInputValidatedProcessorData, SampleInputValidatedProcessorMetaData>
+{
+  public get typeName(): string {
+    return 'SaltMineBuiltInSampleInputValidatedProcessor';
+  }
+
+  public async handleEvent(
+    data: SampleInputValidatedProcessorData,
+    metaData: SampleInputValidatedProcessorMetaData,
+    cfg?: SaltMineConfig
+  ): Promise<void> {
+    Logger.info('Running SampleInputValidatedProcessor, data was : %j, meta was: %j', data, metaData);
+  }
+
+  public get dataSchema(): string {
+    return 'SaltMineSampleInputValidatedProcessorData';
+  }
+
+  public async validateMetaData(input: SampleInputValidatedProcessorMetaData): Promise<string[]> {
+    const rval: string[] = [];
+    if (!input) {
+      rval.push('Metadata is required');
+    }
+    if (!input.metaFieldString || input.metaFieldString.length < 4) {
+      rval.push('metaFieldString must exist and be longer than 4 characters');
+    }
+    return rval;
+  }
+}
+
+export interface SampleInputValidatedProcessorData {
+  nameParam: string;
+  numberParam: number;
+}
+
+export interface SampleInputValidatedProcessorMetaData {
+  metaFieldString: string;
+  metaFieldBoolean: boolean;
+}
--- src/salt-mine/salt-mine-named-processor.ts
+++ src/salt-mine/salt-mine-named-processor.ts
@@ -5,7 +5,7 @@ export interface SaltMineNamedProcessor<T, R> {
   dataSchema?: string; // If set, the data object will be validated against this schema from the OpenAPI doc
   metaDataSchema?: string; // If set, the metadata object will be validated against this schema from the OpenAPI doc
   validateData?(input: T): Promise<string[]>; // If defined, validates the incoming data object with this instead of the OpenAPI doc
-  validateMetaData?(input: T): Promise<string[]>; // If defined, validates the incoming metadata object with this instead of the OpenAPI doc
+  validateMetaData?(input: R): Promise<string[]>; // If defined, validates the incoming metadata object with this instead of the OpenAPI doc
 
   // Returns void since you can't use the result anyway
   handleEvent(data: T, metaData: R, cfg: SaltMineConfig): Promise<void>;
