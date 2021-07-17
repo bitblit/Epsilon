62815ebab9476d1ed98f9df3963e9dd06d425042
Chris Weiss <bitblit@gmail.com>
2021-07-17 12:20:57 -0700

    Adding http

---
+++ src/http/route/http-config.ts
@@ -0,0 +1,40 @@
+import { HandlerFunction } from './handler-function';
+import { AuthorizerFunction } from './authorizer-function';
+import { ErrorProcessorFunction } from './error-processor-function';
+import { WebTokenManipulator } from '../auth/web-token-manipulator';
+import { ModelValidator } from './model-validator';
+import { ApolloServer, CreateHandlerOptions } from 'apollo-server-lambda';
+
+export interface HttpConfig {
+  handlers: Map<string, HandlerFunction<any>>;
+  authorizers?: Map<string, AuthorizerFunction>;
+  errorProcessor?: ErrorProcessorFunction;
+  customCorsHandler?: HandlerFunction<any>;
+  customTimeouts?: Map<string, number>;
+  staticContentPaths?: string[]; // TODO: Implement
+  customExtraHeaders?: Map<string, string>; // TODO: Implement
+  // Allows you to define an error message for errors that escape all the way to epsilon,
+  // preventing information leakage
+  defaultErrorMessage?: string;
+  corsAllowedOrigins?: string;
+  corsAllowedMethods?: string;
+  corsAllowedHeaders?: string;
+  // If you set a web token manipulator, epsilon will auto-parse the Authorization header
+  webTokenManipulator?: WebTokenManipulator;
+  overrideModelValidator?: ModelValidator; // If set, overrides the one auto-created from yaml
+  // Should typically be your stage name, but can be different in weird cases like custom name map
+  // These will be matched case insensitive
+  prefixesToStripBeforeRouteMatch?: string[];
+  requestIdResponseHeaderName?: string;
+
+  disableAutoCORSOptionHandler?: boolean;
+  defaultTimeoutMS?: number;
+  // See EventUtil.fixStillEncodedQueryParams for an explanation here
+  disableAutoFixStillEncodedQueryParams?: boolean;
+  disableAutoAddCorsHeadersToResponses?: boolean;
+  disableCompression?: boolean;
+  // If set, paths matching this are sent to Apollo for Graphql instead
+  apolloRegex?: RegExp;
+  apolloServer?: ApolloServer;
+  apolloCreateHandlerOptions?: CreateHandlerOptions;
+}
