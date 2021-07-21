9a13d1d469dcc971eba88a008816ff31f566a0a9
Chris Weiss <bitblit@gmail.com>
2021-07-21 16:55:57 -0700

    Adding local mode pump

--- src/epsilon-container.ts
+++ src/epsilon-container.ts
@@ -2,6 +2,7 @@ import { EpsilonConfig } from './global/epsilon-config';
 import { EpsilonInstance } from './global/epsilon-instance';
 import { EpsilonConfigParser } from './epsilon-config-parser';
 import { BackgroundManager } from './background/background-manager';
+import { Logger } from '@bitblit/ratchet/dist/common/logger';
 
 /**
  * This class functions as the adapter from a default Lambda function to the handlers exposed via Epsilon
@@ -17,6 +18,15 @@ export class EpsilonContainer {
       localMode
     );
     this._localMode = localMode;
+
+    if (this.localMode) {
+      Logger.info('Local mode specified - starting background pump');
+      this._backgroundManager.localBus().subscribe(async (evt) => {
+        Logger.debug('Processing local background entry : %j', evt);
+        const rval: boolean = await this._epsilonInstance.backgroundHandler.processSingleBackgroundEntry(evt);
+        Logger.info('Processor returned %s', rval);
+      });
+    }
   }
 
   public get epsilonInstance(): EpsilonInstance {
