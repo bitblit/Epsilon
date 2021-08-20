35b8d79b2d9d1458ddebfb6a9b53a3984a23e113
Chris Weiss <bitblit@gmail.com>
2021-08-20 13:42:26 -0700

    Adding more samples

---
+++ src/built-in/background/log-message-background-error-processor.ts
@@ -0,0 +1,10 @@
+import { Logger } from '@bitblit/ratchet/dist/common';
+import { BackgroundErrorProcessor } from '../../config/background/background-error-processor';
+import { InternalBackgroundEntry } from '../../background/internal-background-entry';
+
+// This is just here to test error processor logic (errors are logged anyway)
+export class LogMessageBackgroundErrorProcessor implements BackgroundErrorProcessor {
+  public async handleError(submission: InternalBackgroundEntry<any>, error: Error): Promise<void> {
+    Logger.error('-------- ERROR PROCESSED : %j : %s----', submission, error);
+  }
+}
