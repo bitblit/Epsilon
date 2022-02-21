cd8bfb29c5ccdcd30dd1a5c62fb44520d658d2ee
zach-herridge <zgherridge@gmail.com>
2022-02-21 12:31:52 -0800

    simplify logic

--- src/background/background-dynamo-audit-table.ts
+++ src/background/background-dynamo-audit-table.ts
@@ -9,7 +9,7 @@ export class BackgroundDynamoAuditTable implements BackgroundHandlerListener {
   constructor(private dynamo: DynamoRatchet, private tableName, private backgroundQueueName) {}
 
   async onEvent(event: BackgroundHandlerEvent): Promise<void> {
-    const basicEntry = {
+    const entry = {
       backgroundQueueName: this.backgroundQueueName,
       requestId: ContextUtil.currentRequestId(),
       guid: event.guid,
@@ -18,20 +18,14 @@ export class BackgroundDynamoAuditTable implements BackgroundHandlerListener {
       timestampEpochMs: new Date().getTime(),
     };
 
-    if (event.type == BackgroundHandlerEventType.ProcessStarting) {
-      await this.dynamo.simplePut(this.tableName, basicEntry);
-    } else if (event.type == BackgroundHandlerEventType.NoMatchProcessorName) {
-      await this.dynamo.simplePut(this.tableName, basicEntry);
-    } else if (event.type == BackgroundHandlerEventType.ExecutionSuccessfullyComplete) {
-      await this.dynamo.simplePut(this.tableName, basicEntry);
-    } else if (event.type == BackgroundHandlerEventType.DataValidationError) {
+    if (event.type == BackgroundHandlerEventType.DataValidationError) {
       const errors: string[] = event.data;
-      basicEntry['error'] = errors.join(', ');
-      await this.dynamo.simplePut(this.tableName, basicEntry);
+      entry['error'] = errors.join(', ');
     } else if (event.type == BackgroundHandlerEventType.ExecutionFailedError) {
       const error: Error = event.data;
-      basicEntry['error'] = ErrorRatchet.safeStringifyErr(error);
-      await this.dynamo.simplePut(this.tableName, basicEntry);
+      entry['error'] = ErrorRatchet.safeStringifyErr(error);
     }
+
+    await this.dynamo.simplePut(this.tableName, entry);
   }
 }
