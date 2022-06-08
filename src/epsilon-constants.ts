06b2e2d5a4b006225158b2a861a5eb9386b8f0f4
Christopher Weiss <bitblit@gmail.com>
2022-06-07 20:57:21 -0700

    Checkpoint - trying to add node global handling

--- src/epsilon-constants.ts
+++ src/epsilon-constants.ts
@@ -1,4 +1,10 @@
+import { EpsilonGlobalHandler } from './epsilon-global-handler';
+import { Logger } from '@bitblit/ratchet/dist/common/logger';
+import { ErrorRatchet } from '@bitblit/ratchet/dist/common/error-ratchet';
+
 export class EpsilonConstants {
+  private static readonly EPSILON_GLOBAL_HANDLER_PRODUCER_FIELD_NAME = '___epsilonGlobalHandlerProducer';
+
   public static readonly AUTH_HEADER_PREFIX: string = 'Bearer ';
   public static readonly AUTH_HEADER_NAME: string = 'Authorization';
 
@@ -7,6 +13,20 @@ export class EpsilonConstants {
   public static readonly BACKGROUND_SNS_IMMEDIATE_RUN_FLAG = 'BACKGROUND_IMMEDIATE_RUN_FLAG';
 
   public static readonly INTER_API_SNS_EVENT = 'EPSILON_INTER_API_EVENT';
+
+  public static applyGloballyAvailableEpsilonGlobalHandlerProducer(producer: () => Promise<EpsilonGlobalHandler>): void {
+    if (!global) {
+      ErrorRatchet.throwFormattedErr('Cannot set global - global does not exist');
+    }
+    global[EpsilonConstants.EPSILON_GLOBAL_HANDLER_PRODUCER_FIELD_NAME] = producer;
+  }
+
+  public static async findGloballyAvailableEpsilonGlobalHandler(): Promise<EpsilonGlobalHandler> {
+    Logger.info('Looking for global epsilon global handler');
+    const rval: EpsilonGlobalHandler = global ? global[EpsilonConstants.EPSILON_GLOBAL_HANDLER_PRODUCER_FIELD_NAME] : null;
+    Logger.debug('Found %s', rval);
+    return rval;
+  }
 
   // Prevent instantiation
   // eslint-disable-next-line @typescript-eslint/no-empty-function
