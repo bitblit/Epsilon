264ac07d3ead23c2beb25790cfeb208640882c4b
Christopher Weiss <bitblit@gmail.com>
2022-08-11 15:08:59 -0700

    Move logging

--- src/epsilon-global-handler.ts
+++ src/epsilon-global-handler.ts
@@ -42,11 +42,11 @@ export class EpsilonGlobalHandler {
   public static CURRENT_PROCESS_LABEL: string;
 
   constructor(private _epsilon: EpsilonInstance) {
-    this.configureDefaultLogger();
+    EpsilonGlobalHandler.configureDefaultLogger();
     Logger.info('Default logger configured');
   }
 
-  public configureDefaultLogger(): void {
+  public static configureDefaultLogger(): void {
     Logger.changeDefaultOptions(
       {
         initialLevel: LoggerLevelName.info,
