0547df881be5dab385fc39049ea51263b3bfe6e6
Christopher Weiss <bitblit@gmail.com>
2023-01-04 00:29:56 -0800

    Update and config

--- src/built-in/daemon/daemon-config.ts
+++ src/built-in/daemon/daemon-config.ts
@@ -2,6 +2,8 @@ import { DaemonAuthorizerFunction } from './daemon-authorizer-function';
 import { DaemonGroupSelectionFunction } from './daemon-group-selection-function';
 
 export interface DaemonConfig {
-  authorizer: DaemonAuthorizerFunction;
-  groupSelector: DaemonGroupSelectionFunction;
+  authorizer?: DaemonAuthorizerFunction;
+  groupSelector?: DaemonGroupSelectionFunction;
+  fetchDaemonStatusPathParameter?: string;
+  fetchDaemonStatusByPublicTokenPathParameter?: string;
 }
--- src/built-in/daemon/daemon-handler.ts
+++ src/built-in/daemon/daemon-handler.ts
@@ -5,9 +5,8 @@ import { DaemonLike } from '@bitblit/ratchet/aws';
 import { ExtendedAPIGatewayEvent } from '../../config/http/extended-api-gateway-event';
 import { NotFoundError } from '../../http/error/not-found-error';
 import { DaemonProcessStateList } from './daemon-process-state-list';
-import { DaemonGroupSelectionFunction } from './daemon-group-selection-function';
 import { DaemonConfig } from './daemon-config';
-import { JwtRatchetLike } from '@bitblit/ratchet/common';
+import { StringRatchet } from '@bitblit/ratchet/common';
 
 /**
  * A helper class to simplify adding Ratchet "Daemon" handling to your application
@@ -19,25 +18,30 @@ export class DaemonHandler {
   ) => {
     return true;
   };
-  private groupSelectionFunction: DaemonGroupSelectionFunction;
-  private authorizer: DaemonAuthorizerFunction;
+  private config: DaemonConfig;
+  //private groupSelectionFunction: DaemonGroupSelectionFunction;
+  //private authorizer: DaemonAuthorizerFunction;
 
   /**
    * Initialize the Router
    */
-  constructor(private daemon: DaemonLike, config?: DaemonConfig) {
-    this.authorizer = config?.authorizer || DaemonHandler.ALLOW_EVERYTHING_AUTHORIZER;
-    this.groupSelectionFunction = config?.groupSelector || ((evt: ExtendedAPIGatewayEvent) => Promise.resolve(daemon.defaultGroup));
+  constructor(private daemon: DaemonLike, private inConfig?: DaemonConfig) {
+    this.config = inConfig || {};
+    this.config.authorizer = this.config.authorizer || DaemonHandler.ALLOW_EVERYTHING_AUTHORIZER;
+    this.config.groupSelector = this.config.groupSelector || ((evt: ExtendedAPIGatewayEvent) => Promise.resolve(daemon.defaultGroup));
+    this.config.fetchDaemonStatusByPublicTokenPathParameter =
+      StringRatchet.trimToNull(this.config.fetchDaemonStatusByPublicTokenPathParameter) || 'publicToken';
+    this.config.fetchDaemonStatusPathParameter = StringRatchet.trimToNull(this.config.fetchDaemonStatusPathParameter) || 'key';
   }
 
   // If you are going to map this function, be sure that your Daemon is setup with a JwtRatchet...
   public async fetchDaemonStatusByPublicToken(evt: ExtendedAPIGatewayEvent): Promise<DaemonProcessState> {
     // TODO: verify has access to this key
-    const publicToken: string = evt.pathParameters['publicToken'];
+    const publicToken: string = evt.pathParameters[this.config.fetchDaemonStatusByPublicTokenPathParameter];
     Logger.info('Fetching daemon status for token: %s', publicToken);
 
     let rval: DaemonProcessState = await this.daemon.statFromPublicToken(publicToken);
-    const canRead: boolean = rval ? await this.authorizer(evt, rval) : false;
+    const canRead: boolean = rval ? await this.config.authorizer(evt, rval) : false;
     rval = canRead ? rval : null;
     if (rval === null) {
       throw new NotFoundError('No such token : ' + publicToken);
@@ -47,11 +51,11 @@ export class DaemonHandler {
 
   public async fetchDaemonStatus(evt: ExtendedAPIGatewayEvent): Promise<DaemonProcessState> {
     // TODO: verify has access to this key
-    const daemonKey: string = evt.pathParameters['key'];
+    const daemonKey: string = evt.pathParameters[this.config.fetchDaemonStatusPathParameter];
     Logger.info('Fetching daemon status for : %s', daemonKey);
 
     let rval: DaemonProcessState = await this.daemon.stat(daemonKey);
-    const canRead: boolean = rval ? await this.authorizer(evt, rval) : false;
+    const canRead: boolean = rval ? await this.config.authorizer(evt, rval) : false;
     rval = canRead ? rval : null;
     if (rval === null) {
       throw new NotFoundError('No such key : ' + daemonKey);
@@ -60,11 +64,11 @@ export class DaemonHandler {
   }
 
   public async listDaemonStatus(evt: ExtendedAPIGatewayEvent): Promise<DaemonProcessStateList> {
-    const group: string = await this.groupSelectionFunction(evt);
+    const group: string = await this.config.groupSelector(evt);
     let keys: DaemonProcessState[] = await this.daemon.list(group);
     const allowed: DaemonProcessState[] = [];
     for (let i = 0; i < keys.length; i++) {
-      const canRead: boolean = await this.authorizer(evt, keys[i]);
+      const canRead: boolean = await this.config.authorizer(evt, keys[i]);
       if (canRead) {
         allowed.push(keys[i]);
       }
